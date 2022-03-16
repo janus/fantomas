su8898/fantomless

Remove unneeded parantheses in elif too
Expands on 81d27b524f874d6a9a5b3a0fe5ad77ba5b6359c9
towards https://github.com/fsprojects/fantomas/issues/684 .

src/Fantomas/CodePrinter.fs
@@ -2092,7 +2092,7 @@ and genExpr astContext synExpr ctx =
                 let genElifOneliner (elf1: SynExpr, elf2: SynExpr, elifKeywordRange, thenKeywordRange) =
                     TriviaContext.``else if / elif`` elifKeywordRange
                     +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
-                    +> genExpr astContext elf1
+                    +> genExpr astContext (cleanIfExpr elf1)
                     +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
                     +> genThen thenKeywordRange
                     +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
@@ -2100,7 +2100,7 @@ and genExpr astContext synExpr ctx =
 
                 let genElifMultiLine (elf1: SynExpr, elf2, elifKeywordRange, thenKeywordRange) =
                     (TriviaContext.``else if / elif`` elifKeywordRange)
-                    +> autoIndentAndNlnWhenWriteBeforeNewlineNotEmpty (genExprInIfOrMatch astContext elf1)
+                    +> autoIndentAndNlnWhenWriteBeforeNewlineNotEmpty (genExprInIfOrMatch astContext (cleanIfExpr elf1))
                     +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
                     +> genThen thenKeywordRange
                     +> indent

janus/fantomas

src/Fantomas/CodePrinter.fs
@@ -2301,7 +2301,7 @@ and genExpr astContext synExpr ctx =
                 +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
                 +> genIf ifKw isElif
                 +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
-                +> genExpr astContext e1
+                +> genExpr astContext (cleanIfExpr e1)
                 +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
                 +> genThen thenKw
                 +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
@@ -2311,7 +2311,7 @@ and genExpr astContext synExpr ctx =
                 optSingle genElse elseKw
                 +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
                 +> genIf ifKw isElif
-                +> autoIndentAndNlnWhenWriteBeforeNewlineNotEmpty (genExprInIfOrMatch astContext e1 false)
+                +> autoIndentAndNlnWhenWriteBeforeNewlineNotEmpty (genExprInIfOrMatch astContext (cleanIfExpr e1) false)
                 +> sepNlnWhenWriteBeforeNewlineNotEmpty sepSpace
                 +> genThen thenKw
                 +> indent